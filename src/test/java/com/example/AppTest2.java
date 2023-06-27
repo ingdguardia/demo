84df9c8119ffb5a58fd92c946770a245e667f836
dario.guardia <dario.guardia@ip-192-168-0-102.sa-east-1.compute.internal>
2023-06-27 16:09:37 -0300

    test2

--- src/test/java/com/example/AppTest2.java
+++ src/test/java/com/example/AppTest2.java
@@ -7,21 +7,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class AppTest {
+public class AppTest2 {
 
     @Test
-    public void testApp() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("https://telebajocero.backend.thinkindot.com/backend/");
-        System.out.println("TEST 1");
-        Thread.sleep(2000);
-        driver.quit();
-    }
-
-    // @Test
     public void testApp2() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
