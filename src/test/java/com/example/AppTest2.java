30ccd268d9141542e97aa69b2c074ffdc7430a43
dario.guardia <dario.guardia@ip-192-168-0-102.sa-east-1.compute.internal>
2023-06-28 10:35:20 -0300

    fix

--- src/test/java/com/example/AppTest2.java
+++ src/test/java/com/example/AppTest2.java
@@ -1,8 +1,8 @@
 package com.example;
 
-import org.testng.annotations.AfterSuite;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.annotations.AfterSuite;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,16 +10,23 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AppTest2 {
+    String urlLogin = "https://telebajocero.backend.thinkindot.com/backend/";
+    String urlAdminNotas = "https://telebajocero.backend.thinkindot.com/backend/administrator/notas";
 
     @Test
     public void testApp2() throws InterruptedException {
+        System.out.println("TEST 2");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://telebajocero.backend.thinkindot.com/backend/");
-        System.out.println("TEST 2");
-        Thread.sleep(4000);
+        Assert.assertEquals(urlLogin, driver.getCurrentUrl());
+        driver.findElement(By.id("usuario")).sendKeys("dguardia");
+        driver.findElement(By.id("password")).sendKeys("dguardia");
+        driver.findElement(By.xpath("//a[text()='Ingresar']")).click();
+        Assert.assertEquals(urlAdminNotas, driver.getCurrentUrl());
+        System.out.println("TEST 2 PASSED");
         driver.quit();
     }
 
