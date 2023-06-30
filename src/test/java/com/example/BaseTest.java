23a455bb9156abf2ba4ea146a59d84394226a54c
dario.guardia <dario.guardia@ip-192-168-0-102.sa-east-1.compute.internal>
2023-06-30 16:02:17 -0300

    fix thread

--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -33,4 +33,16 @@ public class BaseTest {
         driver.quit();
     }
 
+    @Test
+    public void testApp() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://telebajocero.backend.thinkindot.com/backend/");
+        System.out.println("TEST 1");
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
 }
