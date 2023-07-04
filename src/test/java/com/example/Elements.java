97a36587bbf49adbec301fc721bc4b00733d6ee7
dario.guardia <dario.guardia@ip-192-168-0-102.sa-east-1.compute.internal>
2023-07-04 16:09:29 -0300

    fix frame

--- src/test/java/com/example/Elements.java
+++ src/test/java/com/example/Elements.java
@@ -28,7 +28,7 @@ public class Elements {
     By txtVolantaNota = By.id("volanta");
     By txtTituloNota = By.id("titulo");
     By txtCopeteNota = By.id("copete");
-    By txtCuerpoNota = By.cssSelector("#mce_0 > p");
+    By txtCuerpoNota = By.xpath("//*[@id='mce_0']/p");
     By btnGrabarNota = By.id("save-button");
     By headerNota = By.xpath("//h2[text()='" + tituloNota + "']");
 
