8e466ff156fb25a5d3860e24f47dc72e9627868c
dario.guardia <dario.guardia@ip-192-168-0-102.sa-east-1.compute.internal>
2023-07-05 16:57:56 -0300

    feat api

--- src/test/java/com/example/Elements.java
+++ src/test/java/com/example/Elements.java
@@ -1,21 +1,12 @@
 package com.example;
 
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Elements {
     String tituloNota;
--- src/test/java/com/example/FullTest.java
+++ src/test/java/com/example/FullTest.java
@@ -5,18 +5,12 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.ArrayList;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -96,6 +90,7 @@ public class FullTest extends Elements {
             visibiltyOf(driver, headerNota);
             String testApi = urlTestApi + idApi;
             driver.get(testApi);
+            System.out.println(testApi);
             Thread.sleep(3000);
             System.out.println("CREAR NOTA OK");
 
