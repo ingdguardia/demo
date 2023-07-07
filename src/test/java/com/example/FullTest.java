b396f3aaff072569bbe2fe9f7b16c4bb688158f3
dario.guardia <dario.guardia@ip-192-168-0-102.sa-east-1.compute.internal>
2023-07-07 15:57:11 -0300

    fix body?

--- src/test/java/com/example/FullTest.java
+++ src/test/java/com/example/FullTest.java
@@ -118,7 +118,9 @@ public class FullTest extends Elements {
             driver.switchTo().window(tabs.get(1));
             checkTitlePage(driver, "Sin Titulo");
             Thread.sleep(5000);
+            driver.switchTo().frame("cuerpoiframe");
             sendKeys(driver, inputCuerpoNota, "TEST CUERPO");
+            driver.switchTo().frame("editorIframe");
             sendKeys(driver, inputVolantaNota, volanta);
             sendKeys(driver, inputTituloNota, titulo);
             sendKeys(driver, inputCopeteNota, copete);
