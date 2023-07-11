cf22e339f38c87e7d3cc3cead44189ca7245f3fa
dario.guardia <dario.guardia@ip-192-168-0-102.sa-east-1.compute.internal>
2023-07-11 12:44:45 -0300

    test file

--- src/test/java/com/example/FullTest.java
+++ src/test/java/com/example/FullTest.java
@@ -361,8 +361,14 @@ public class FullTest extends Elements {
             driver.switchTo().window(tabs.get(2));
             Thread.sleep(3000);
             // driver.switchTo().frame("subEditorIframe");
-            driver.findElement(btnArchivos).click();
+            // driver.findElement(btnFile).click();
             // clickJS(driver, btnFile);
+            try {
+                driver.findElement(btnFile).click();
+
+            } catch (Exception e) {
+                System.out.println("ERROR: " + e);
+            }
             sendKeys(driver, btnFile, link);
             optionTipoAdjunto = By.xpath("(//li//span[text()='" + tipo + "'])[1]");
             clickJS(driver, dropdownTipoAdjunto);
